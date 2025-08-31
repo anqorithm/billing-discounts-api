6ba22af0064936522e3472a9b4eb3c6941c8cb6b
Abdullah Alqahtani <anqorithm@protonmail.com>
2025-08-31 16:59:25 +0300

    init: first commit

---
+++ billing-discounts-api/src/test/java/sa/billing/discounts/BillingDiscountsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.billing.discounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingDiscountsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
